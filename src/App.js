16867006391adf8ddb06a970b0cf4d2846a36231
Kevin Tsai <k.tsai92@gmail.com>
2019-12-25 22:58:56 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React from 'react';
 import './App.css';
 import Form from './Components/Form.js';
 import  Titles from'./Components/Titles.js';
-import Weather from './Components/Weather.js';
+//import Weather from './Components/Weather.js';
 
-function App() {
-  return (
-    <div>
-      <Titles/>
-      <Form/>
-    </div>
-  )
+const Api_Key = 'e90c6bb8-bfbd-11e9-9cb5-2a2ae2dbcce4';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: [],
+      country: [],
+    }
+  }
+/*
+  componentDidMount() {
+    this.getWeather();
+  }
+*/
+  getWeather = (e) => {
+    const {city, country} = this.state;
+    e.preventDefault();
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${Api_Key}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <Titles/>
+        <Form loadWeather = {this.getWeather}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,14 +3,14 @@ import "./Form.css"
 class Form extends React.Component {
 	render() {
 		return(
-				<form>
+				<form onSubmit = {this.props.loadWeather}>
 					<label>
 						City:
-						<input type="text" name="city" class="city"/>
+						<input type="text" name="city" className="city"/>
 					</label>
 					<label>
 						Country:
-						<input type ="text" name="country" class="country"/>
+						<input type ="text" name="country" className="country"/>
 					</label>
 					<input type ="submit" value="Submit" />
 				</form>
