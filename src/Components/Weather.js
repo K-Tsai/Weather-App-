9aba38acfd3747560c4a5fa7244bbdfd189b0016
Kevin Tsai <k.tsai92@gmail.com>
2020-01-16 15:14:38 -0500

    added some code

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import "./Weather.css";
 
 class Weather extends React.Component {
   render() {
     return (
 			<div className= "results">
-      	<p className= "col"> {this.props.city}</p>
-				<p className= "col"> {this.props.country}</p>
-				<p> {this.props.temperature}</p>
-				<p> {this.props.humidity}</p>
-				<p> {this.props.description}</p>
+				<div className= "results1">
+      		<p className= "col"> {this.props.city}</p>
+					<p className= "col"> {this.props.country}</p>
+					<p> {this.props.temperature}</p>
+					<p> {this.props.humidity}</p>
+					<p> {this.props.description}</p>
+					</div>
 			</div>
     )
   }
